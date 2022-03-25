64278189aaeb553c9085e3e8c962ab84f12baaa5
AhsanMustafa665 <ahsanfaisal665@gmail.com>
2022-03-26 00:48:58 +0600

    fontAweosome and favIcon added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Musks from './Components/Musks/Musks';
 function App() {
   return (
     <div className="App">
-      <h1>Covid-19 & Extra Protection Musk</h1>
-      <h2>Select 4 Musks here.</h2>
+      <h1 style={{ color: 'green', fontFamily: 'cursive', fontSize: '48px' }}>Covid-19 & Extra Protection Musk</h1>
+      <h2 style={{ color: 'indigo', fontSize: '40px' }}>Select 4 Musks here.</h2>
       <Musks></Musks>
-    </div>
+    </div >
   );
 }
 
--- src/Components/Musk/Musk.js
+++ src/Components/Musk/Musk.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './Musk.css';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping, faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Musk = (props) => {
     const { name, img, price, service } = props.musk;
     return (
-        <div>
-            <div className='product'>
-                <img src={img} alt="" />
+
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
                 <h2>{name}</h2>
                 <p>{service}</p>
                 <h4>Price : ${price}</h4>
             </div>
-
+            <button className='btn-cart'>ADD TO CART
+                <FontAwesomeIcon className='font' icon={faCartShopping}></FontAwesomeIcon> </button>
         </div>
+
+
     );
 };
 
